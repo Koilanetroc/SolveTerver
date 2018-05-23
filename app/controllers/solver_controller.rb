54fe3213bac65533aa41bc6b183fd447958618a9
OlegT <koilanetroc@gmail.com>
2018-05-23 19:36:58 +0700

    finished 1,3,4,5,6,7 tasks

--- app/controllers/solver_controller.rb
+++ app/controllers/solver_controller.rb
@@ -7,11 +7,40 @@ class SolverController < ApplicationController
   end
 
   def solve
+    # Task 1
     @X = params['value_x'].strip.split(',').map(&:to_f)
     @Y = params['value_y'].strip.split(',').map(&:to_f)
+    @N = @X.size
     @step = ((@X.max - @X.min) / (1 + 3.322 * Math.log10(100))).roundf(3)
+    # Task 2
     @intervals = compute_intervals(@X, @step)
-    puts @step
+    # Task 3
+    @X_vec = (@X.inject(0) { |sum, x| sum + x } / @N.to_f).roundf(3)
+    @Y_vec = (@Y.inject(0) { |sum, x| sum + x } / @N.to_f).roundf(3)
+    @fixed_dispersion_X = ((1 / (@N - 1).to_f) * @X.inject(0) { |sum, x| sum + (x**2 - @X_vec.to_f**2) }).roundf(3)
+    @fixed_dispersion_Y = ((1 / (@N - 1).to_f) * @Y.inject(0) { |sum, y| sum + (y**2 - @Y_vec.to_f**2) }).roundf(3)
+    @deviation_X = (@fixed_dispersion_X**0.5).roundf(3)
+    @deviation_Y = (@fixed_dispersion_Y**0.5).roundf(3)
+    # Task 4
+    @intervals[0][:left] = '-inf'
+    @intervals.last[:right] = 'inf'
+    @intervals_2 = compute_laplases(@intervals, @X_vec, @deviation_X)
+    reshaped_intervals = reshape_intervals(@intervals_2, @N, @X_vec, @deviation_X)
+    @intervals_3 = compute_frequencies2(reshaped_intervals, @X, @N)
+    @xi_pirsona = 0
+    @intervals_3.each { |interval| @xi_pirsona += interval[:xi_pirsona] }
+    @xi_pirsona = @xi_pirsona.roundf(3)
+    @xi_k = XI_K[@intervals_3.size - 3]
+    # Task 5
+    @beta = (T_GAMMA * @deviation_X) / @N**0.5
+    @q = Q
+    # Task 6
+    sum = 0
+    @X.each_with_index { |_x, i| sum += (@X[i] * @Y[i] - @X_vec * @Y_vec) }
+    @r_v = ((sum * 1 / @N) / (@deviation_X * @deviation_Y)).roundf(4)
+    # Task 7
+    @t_v = (@r_v * (@N - 2)**0.5) / (1 - @r_v**2)**0.5
+    @t_alfa = T_ALFA
     render 'solve'
   end
 end
--- app/helpers/solver_helper.rb
+++ app/helpers/solver_helper.rb
@@ -15,5 +15,108 @@ module SolverHelper
       right += step
     end
     intervals
+  end
+
+  def compute_laplases(full_intervals, x_vec, deviation_X)
+    full_intervals.each do |interval|
+      left = interval[:left].to_s.include?('inf') ? laplas(interval[:left]) : laplas((interval[:left] - x_vec) / deviation_X) { |x| f(x) }
+      right = interval[:right].to_s.include?('inf') ? laplas(interval[:right]) : laplas((interval[:right] - x_vec) / deviation_X) { |x| f(x) }
+      interval[:laplas] = (right - left).roundf(3)
+    end
+    full_intervals
+  end
+
+  def laplas(_raw_x1)
+    return -0.5 if _raw_x1.to_s.eql? '-inf'
+    return 0.5 if _raw_x1.to_s.eql? 'inf'
+    x1 = _raw_x1.abs
+    dx = x1 / 1000.0
+    x = 0
+    sum = 0
+    loop do
+      y = yield(x)
+      sum += dx * y
+      x += dx
+      break if x > x1
+    end
+    answer = sum * (1 / (2 * Math::PI)**0.5)
+    if _raw_x1 < 0.0
+      answer.roundf(3) * -1
+    else
+      answer.roundf(3)
+    end
+  end
+
+  def f(_t)
+    Math.exp((-_t**2) / 2)
+  end
+
+  def reshape_intervals(intervals, n, _x_vec, _deviation_x)
+    new_intervals = []
+    left = nil
+    right = nil
+    intervals.each_with_index do |interval, i|
+      left = interval[:left] if left.nil?
+      right = interval[:right] if right.nil?
+      if compute_P(left, right, _x_vec, _deviation_x) * n > 10 || right.to_s.eql?('inf')
+        if compute_P(intervals[i + 1][:left], 'inf', _x_vec, _deviation_x) * n > 10
+          new_intervals.push(left: left, right: right, laplas: compute_P(left, interval[:right], _x_vec, _deviation_x))
+          left = nil
+          right = nil
+        else
+          new_intervals.push(left: left, right: 'inf', laplas: compute_P(left, 'inf', _x_vec, _deviation_x))
+          left = nil
+          right = nil
+          break
+        end
+      else
+        right = nil
+      end
+    end
+    new_intervals
+  end
+
+  def compute_frequencies2(_intervals, x, n)
+    _intervals.each do |_interval|
+      n_i = 0
+      x.each do |v|
+        if _interval[:left].to_s.eql? '-inf'
+          n_i += 1 if v <= _interval[:right]
+        elsif _interval[:right].to_s.eql? 'inf'
+          n_i += 1 if v > _interval[:left]
+        else
+          n_i += 1 if (v > _interval[:left]) && (v <= _interval[:right])
+        end
+      end
+      _interval[:n_i] = n_i
+      _interval[:p_i] = _interval[:laplas]
+      _interval[:n_p_i] = (_interval[:laplas] * n).roundf(3)
+      _interval[:xi_pirsona] = (((n_i - n * _interval[:laplas])**2) / (n * _interval[:laplas])).roundf(3)
+    end
+    _intervals
+  end
+
+  def compute_P(_from, _to, _x_vec, _deviation_x)
+    from = _from.to_s.include?('inf') ? laplas(_from) : laplas((_from - _x_vec) / _deviation_x) { |x| f(x) }
+    to = _to.to_s.include?('inf') ? laplas(_to) : laplas((_to - _x_vec) / _deviation_x) { |x| f(x) }
+    (to - from).roundf(3)
+  end
+
+  XI_K = {
+    1 => 3.8415,
+    2 => 5.991,
+    3 => 7.8147,
+    4 => 9.4877
+  }.freeze
+  T_GAMMA = 1.984
+  Q = 0.143
+  T_ALFA = 1.98
 end
+
+# КОстыль костылей госпаде
+class Float
+  def roundf(places)
+    temp = to_s.length
+    format("%#{temp}.#{places}f", self).to_f
+  end
 end
